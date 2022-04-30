00e0d88027c09de68e280254dd66fecd2d81e885
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-04-30 17:13:04 -0300

    nodemon e node modules installed

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* 
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -11,4 +12,35 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+});
+*/
+
+const express = require("express");
+const app = express();
+
+
+app.get("/",function(req,res){
+    res.send("Ola pessoa.")
+})
+
+app.get("/sobre",function(req,res){
+    res.send("Eu sou o Pedro e estou estudando Node js para desenvolvimento back end")
+})
+
+app.get("/pessoa/:nome/:cidade",function(req,res){
+    res.send("<h1>Bem vindo " + req.params.nome + "</h1>" + "<h2>Pelo visto você é de: " + req.params.cidade + "</h2>");
+
+
+})
+app.get("/:idade",function(req,res){
+    res.send("Você tem " + req.params.idade + " meus parabéns");
+
+
+})
+
+
+
+
+app.listen(3000, function(){
+    console.log("Acesse o servidor em http://localhost:3000");
 });
